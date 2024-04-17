cb9e697147bfe85264d2e15fbd3080e792a2873f
unknown <vuquanganh2201@gmail.com>
2024-04-17 10:40:13 +0700

    first commit

---
+++ gym_robot_arm/envs/__init__.py
@@ -0,0 +1,2 @@
+from gym_robot_arm.envs.robot_arm_env import RobotArmEnvV0, RobotArmEnvV1
+from gym_robot_arm.envs.robot_3arm_env import RobotArmEnvV3
---
+++ test.py
@@ -0,0 +1,16 @@
+import gym
+import time
+env = gym.make('gym_robot_arm:robot-arm-v3')
+for i_episode in range(1):
+    observation = env.reset()
+    for t in range(100):
+        env.render()
+        print(observation)
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        time.sleep(1)
+        print(reward)
+        if done:
+            print("Episode finished after {} timesteps".format(t + 1))
+            break
+env.close()
